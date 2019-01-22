59a3a07fd3a962fbf27562d88f76fdff106784d5
joao <joao182cx@gmail.com>
2019-01-21 22:22:19 -0200

    Um Color currentColor que representa a cor atual de desenho.

--- src/DesenhaPanel.java
+++ src/DesenhaPanel.java
@@ -1,9 +1,13 @@
+import java.awt.Color;
 
-public class DesenhaPanel {
+import javax.swing.JPanel;
+
+public class DesenhaPanel extends JPanel {
 	
 	private MinhaForma[] formas;
 	private int contagemFormas = 0;
 	private int tipoForma = 0;
 	private MinhaForma formaAtual;
+	private Color corAtual;
 	
 }
