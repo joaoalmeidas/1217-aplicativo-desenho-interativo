5017baf433c796bd7cdc6029d952ed06b980f4cc
joao <joao182cx@gmail.com>
2019-01-21 23:51:21 -0200

    Adição dos Getters e Setters.

--- src/DesenhaPanel.java
+++ src/DesenhaPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Graphics;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,5 +13,83 @@ public class DesenhaPanel extends JPanel {
 	private Color corAtual;
 	private boolean preenchido;
 	private JLabel labelStatus;
+	
+	
+	public MinhaForma[] getFormas() {
+		return formas;
+	}
+	
+	
+	public void setFormas(MinhaForma[] formas) {
+		this.formas = formas;
+	}
+	
+	
+	public int getContagemFormas() {
+		return contagemFormas;
+	}
+	
+	
+	public void setContagemFormas(int contagemFormas) {
+		this.contagemFormas = contagemFormas;
+	}
+	
+	
+	public int getTipoForma() {
+		return tipoForma;
+	}
+	
+	
+	public void setTipoForma(int tipoForma) {
+		this.tipoForma = tipoForma;
+	}
+	
+	
+	public MinhaForma getFormaAtual() {
+		return formaAtual;
+	}
+	
+	
+	public void setFormaAtual(MinhaForma formaAtual) {
+		this.formaAtual = formaAtual;
+	}
+	
+	
+	public Color getCorAtual() {
+		return corAtual;
+	}
+	
+	
+	public void setCorAtual(Color corAtual) {
+		this.corAtual = corAtual;
+	}
+	
+	
+	public boolean isPreenchido() {
+		return preenchido;
+	}
+	
+	
+	public void setPreenchido(boolean preenchido) {
+		this.preenchido = preenchido;
+	}
+	
+	
+	public JLabel getLabelStatus() {
+		return labelStatus;
+	}
+	
+	
+	public void setLabelStatus(JLabel labelStatus) {
+		this.labelStatus = labelStatus;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
